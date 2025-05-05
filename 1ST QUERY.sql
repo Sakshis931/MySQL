8acd43dccfd23c3f05bb3350023e204d69982655
Sakshis931 <sakshi931singh@gmail.com>
2025-05-05 14:02:39 +0530

    Add files via upload

---
+++ 1ST QUERY.sql
@@ -0,0 +1,17 @@
+create database samtest;
+
+show databases;
+drop database samtest;
+show databases;
+create database samtest;
+show databases;
+use samtest;
+
+create table stud(
+sid int, sname char(10),sper float,grades char(1)
+);
+create table employee1(
+empid int,empname char(10),department char(15),salary float
+);
+SHOW DATABASES;
+DROP TABLE EMPLOYEE;
